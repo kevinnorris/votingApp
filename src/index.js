a1672dc6dce0b4977c6894796c2693e2b37e5cfe
Kevin N <kevin.norris.90@gmail.com>
2017-03-02 11:25:42 +1300

    Added forced ssl in production

--- src/index.js
+++ src/index.js
@@ -17,6 +17,17 @@ require('./config/passport')(passport);
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI);
 
+const forceSsl = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect(['https://', req.get('Host'), req.url].join(''));
+  } else {
+    next();
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(forceSsl);
+}
 
 // add body parsing
 app.use(bodyParser.json()); // for parsing application/json
